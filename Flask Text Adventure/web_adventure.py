dc36de05339f3d132cb21e8609a08a9f34b408c9
Conor <conortehan@hotmail.com>
2024-11-10 12:29:59 +0000

    commit and push to github

--- Flask Text Adventure/web_adventure.py
+++ Flask Text Adventure/web_adventure.py
@@ -2,7 +2,34 @@ from flask import Flask, render_template, jsonify, session
 
 app = Flask(__name__)
 
-@app.route("/")
+# Game Data
+location = {
+    'entrance':{
+            'name': 'The Temple Entrance',
+            'description': 'A grand archway leads into a dark and mysterious temple.',
+            'directions':{'north': 'hallway'},
+        },
+        'hallway': {
+            'name': 'a dimly lit Hallway',
+            'description': 'Torches flicker on the walls, casting long shadows. There are doors to the east and west.',
+            'directions': {'east': 'chamber', 'west': 'treasury', 'south': 'entrance'},
+        },
+        'chamber': {
+            'name': 'The Treasury',
+            'description': 'This is where the legendary teasure is said to be hidden!',
+            'directions': {'east': 'hallway'},
+            # Add treasure and challenges here later
+
+        'hallway':{
+            'name': 'The legendary treasure'
+            'description': 'You enter a large open room with a chest directly in the centre of the room with only the light from the moon ligthin the ground in front of you, unaware of the dangers lurking in front of you'
+            'directions': {'west': 'hallway', 'north': 'mysterious statue', 'south': 'an open chest'},
+        }
+        },
+    }
+
+# --- Flask Routes ---
+@app.route("/", method = ["GET", "POST"])
 def index():
     return render_template("index.html")
 
